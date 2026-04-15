4795c1cdd9ba80c5be3bb2c7798407864b6e3c0f
Jose Miguel Gil <jmiguelgil@unicesar.edu.co>
2026-04-15 09:19:18 -0500

    Primer commit del proyecto restaurant

---
+++ src/main/java/com/mycompany/restaurant/Producto.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author salainformatica
+ */
+public class Producto {
+   private final String nombre;
+   private final double precio;
+   private int cantidad;
+
+    public Producto(String nombre, double precio, int cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+   
+   
+   
+}
